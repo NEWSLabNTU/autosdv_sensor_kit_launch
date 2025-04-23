3290aed4e5bb7a47c6f9065409f738aeae053a28
aeon <jerry73204@gmail.com>
2025-04-23 08:30:51 +0800

    Fix pointcloud_preprocessor.launch.py compatibility with Autoware 2025.02.

--- f1eighth_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ f1eighth_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -27,8 +27,8 @@ from launch_ros.descriptions import ComposableNode
 def launch_setup(context, *args, **kwargs):
     # set concat filter as a component
     concat_component = ComposableNode(
-        package="pointcloud_preprocessor",
-        plugin="pointcloud_preprocessor::PointCloudConcatenateDataSynchronizerComponent",
+        package="autoware_pointcloud_preprocessor",
+        plugin="autoware::pointcloud_preprocessor::PointCloudConcatenateDataSynchronizerComponent",
         name="concatenate_data",
         remappings=[
             ("~/input/twist", "/sensing/vehicle_velocity_converter/twist_with_covariance"),
@@ -37,8 +37,8 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             {
                 "input_topics": [
-                    "/sensing/lidar/bf_lidar/points_raw",
-                    "/sensing/lidar/bf_lidar/points_raw",
+                    "/sensing/lidar/iv_points",
+                    "/sensing/lidar/iv_points",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
                 "input_twist_topic_type": "twist",
